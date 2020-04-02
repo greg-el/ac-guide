ec130e94f58b4571a5176e0c26fea242d3b92c91
Greg Leonard <gr3gl@hotmail.com>
2020-04-02 01:23:09 +0100

    updated logic and json dump

--- fish-test.py
+++ fish-test.py
@@ -7,13 +7,24 @@ with open('./fish.json') as f:
     data = json.load(f)
 
 test = data['regions']['southern']
+print("You can currently catch:")
 for item in test:
+    #print(item)
     months = item['months']
     hours = item['time']
-    print(hours['start'], hours['end'])
+    #print(item)
+    #print(hours['start'], hours['end'])
+    #print(months[0]['start'], months[0]['end'])
+    
     if len(months) == 1:
-        if months['start'] < month and month < months['end'] and hours['start'] < hour and hour < hours['end']:
-            print(item['name'])
+        if months[0]['start'] < month and month < months[0]['end']:
+            #print(item)
+            if hours['start'] > hours['end']:
+                if hours['start'] > hour and hour <= 24 or  hour < hours['end']:
+                    print(f"{item['name']} at the {item['location']}")
+            else:
+                if hours['start'] < hour and hours['end'] > hour:
+                    print(f"Y{item['name']} at the {item['location']}")
         
     
 
