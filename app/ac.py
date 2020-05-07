03e35f96ecfd28664ed9621188d4122e515628b9
Greg Leonard <gr3gl@hotmail.com>
2020-05-07 02:47:36 +0100

    updated database url

--- app/ac.py
+++ app/ac.py
@@ -20,7 +20,7 @@ cred = credentials.Certificate({"type": "service_account",
 ac_firebase = firebase_admin.initialize_app(cred)
 
 
-mypool = psycopg2.pool.ThreadedConnectionPool(1, 20, database="acguide", sslmode='require')
+mypool = psycopg2.pool.ThreadedConnectionPool(1, 20, database=os.environ['DATABASE_URL'], sslmode='require')
 
 import routes
 
