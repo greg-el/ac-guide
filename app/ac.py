72dff50a33e52a3925d762681b443816bdb7a689
Greg Leonard <gr3gl@hotmail.com>
2020-05-07 02:25:04 +0100

    updated vars to work on heroku

--- app/ac.py
+++ app/ac.py
@@ -6,13 +6,17 @@ from firebase_admin import credentials
 import psycopg2.pool
 import psycopg2
 import os
+import re
 
 
 app = Flask(__name__, static_folder='./static',instance_relative_config=True)
 app.config.from_object('config.DevelopmentConfig')
 
 #Firebase init
-cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+cred = credentials.Certificate({"type": "service_account",
+"private_key": os.environ['PRIVATE_KEY'].replace('\\n', '\n'),
+"client_email": os.environ['CLIENT_EMAIL'],
+"token_uri": "https://oauth2.googleapis.com/token"})
 ac_firebase = firebase_admin.initialize_app(cred)
 
 #def getconn():
